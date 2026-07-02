b9f8b2988af72e1d8dc2a33a53491e2f069902b9
Mirowaiskhan <57292171+Mirowaiskhan@users.noreply.github.com>
2026-07-01 23:49:08 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/owais-cyber-portfolio/",
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: [
